54d3d057c6a71657e8b1c1f7f5b74b0ad58306f3
Fiston MUNYAMPETA <38526078+mfiston2020@users.noreply.github.com>
2023-03-07 10:06:37 +0200

    controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [UserController::class,'getAllUsers']);
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/user', function () {
     return view('user');
